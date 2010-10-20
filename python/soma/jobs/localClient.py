5ad2690355907ab30c1e039625282db04d5cb172
cointepas <cointepas@613412ce-1361-4b7a-93bf-a3c75af1bae4>
2010-10-20 15:55:20 +0000

    wip

--- python/soma/jobs/localClient.py
+++ python/soma/jobs/localClient.py
@@ -8,17 +8,19 @@
 __docformat__ = "epytext en"
 
 from tempfile import mkstemp
-import os, pickle, signal, errno
+import sys, os, pickle, signal, errno
 from subprocess import Popen, STDOUT
+from time import sleep
+from functools import partial
 from soma.jobs import constants
 
 
-class LocalJobs( self ):
+class LocalJobs( object ):
   def __init__( self, directory ):
     self.directory = directory
   
 
-  def create( self, command, stdin, stdout, stderr, description, working_directory ):
+  def create( self, command, stdin=None, stdout=None, stderr=None, description='', working_directory=None ):
     fd, name = mkstemp( dir=self.directory, prefix='job_' )
     os.close( fd )
     status = { 'command': command,
@@ -29,7 +31,8 @@ class LocalJobs( self ):
       'working_directory': working_directory,
       'return_file': None,
       'pid': None,
-      'status': constants.NOT_SUBMITED,
+      'status': constants.NOT_SUBMITTED,
+      'return_value': None,
     }
     pickle.dump( status, open( name, 'wb' ) )
     return name
@@ -41,21 +44,37 @@ class LocalJobs( self ):
     fd, return_file = mkstemp( dir=self.directory, prefix='return_' )
     os.close( fd )
     status[ 'return_file' ] = return_file
-    args = [ sys.executable, '-c', 'import os; print >> open( "%(return_file)s", "a" ), os.spawnv( os.P_WAIT, "%(program)s", %(args)s )' % { 'return_file': return_file, 'program': self.command[0], 'parameters': repr( command ) } ]
+    command = status[ 'command' ]
+    
+    args = [ sys.executable, '-u', '-c', 'import os; print >> open( "%(return_file)s", "a" ), os.spawnv( os.P_WAIT, "%(program)s", %(args)s )' % { 'return_file': return_file, 'program': command[0], 'args': repr( command ) } ]
     stdin = status[ 'stdin' ]
     if stdin:
       stdin = os.open( stdin, os.O_RDONLY )
+    else:
+      stdin = os.open( '/dev/null', os.O_RDONLY ) # Unix only !
     stdout = status[ 'stdout' ]
     if stdout:
       stdout = os.open( stdout, os.O_WRONLY | os.O_CREAT )
+    else:
+      stdout = os.open( '/dev/null', os.O_WRONLY ) # Unix only !
     stderr = status[ 'stderr' ]
     if stderr:
       if stderr == status[ 'stdout' ]:
-        stderr = STDOUT
+        stderr = stdout
       else:
-        stderr os.open( stderr, os.O_WRONLY | os.O_CREAT )
-    popen = Popen( args, stdin=stdin, stdout=stdout, stderr=stderr, cwd=status[ 'working_directory' ] )
-    status[ 'pid' ] = popen.pid
+        stderr = os.open( stderr, os.O_WRONLY | os.O_CREAT )
+    else:
+      stderr = os.open( '/dev/null', os.O_WRONLY ) # Unix only !
+    cpid = os.fork()
+    if cpid:
+      os.waitpid( cpid, 0 )
+      status = pickle.load( open( status_file )  )
+      pid = status[ 'pid' ]
+    else:
+      popen = Popen( args, stdin=stdin, stdout=stdout, stderr=stderr, cwd=status[ 'working_directory' ], close_fds=True ) # ! Unix only
+    # status[ 'pid' ] = popen.pid
+    status[ 'pid' ] = os.spawnv( os.P_NOWAITO, args[0], args ) # ! Unix only
+    status[ 'status' ] = constants.RUNNING
     pickle.dump( status, open( status_file, 'wb' ) ) 
     if stdin:
       os.close( stdin )
@@ -95,8 +114,12 @@ class LocalJobs( self ):
     return signal_sent
   
   
-  def wait( self, job_id, timeout=-1 ):
-    raise NotImplementedError()
+  def wait( self, job_id, timeout=-1, callback=partial( sleep, 1 ) ):
+    status = jobs.status( job_id )
+    while status[ 0 ] == constants.RUNNING:
+      if callback(): break
+      status = jobs.status( job_id )
+    return status
 
 
   def stop( self, job_id ):
@@ -107,6 +130,60 @@ class LocalJobs( self ):
     raise NotImplementedError()
   
   
+  @staticmethod
+  def isRunning( pid ):
+    # Check if process still exists
+    try:
+      os.getpgid( pid ) # Unix only !
+    except OSError, e:
+      if e.errno == errno.ESRCH: # ESRCH <=> 'No such process'
+        return False
+      else:
+        raise
+    return True
+  
+  
   def status( self, job_id ):
-    raise NotImplementedError()
+    status_file = os.path.join( self.directory, job_id )
+    status = pickle.load( open( status_file )  )
+    status_modified = False
+    pid = status[ 'pid' ]
+    if pid is not None:
+      if not self.isRunning( pid ):
+        pid = None
+        status[ 'pid' ] = None
+        status_modified = True
+    s = status[ 'status' ]
+    if pid:
+      if s == constants.NOT_SUBMITTED:
+        s = constants.RUNNING
+        status[ 'status' ] = s
+        status_modified = True
+    else:
+      if s == constants.RUNNING:
+        status_modified = True
+        s = constants.EXIT_UNDETERMINED
+        f = status[ 'return_file' ]
+        if os.path.exists( f ):
+          l = open( f ).read()
+          if l:
+            return_value = int( l )
+            if return_value < 0:
+              status[ 'return_value' ] = return_value
+              s = constants.FINISHED_TERM_SIG
+            else:
+              status[ 'return_value' ] = return_value
+              s = constants.FINISHED_REGULARLY
+        status[ 'status' ] = s
+    if status_modified:
+      pickle.dump( status, open( status_file, 'wb' ) ) 
+    return ( s, pid, status[ 'return_value' ] )
+
+
+if __name__ == '__main__':
+  jobs = LocalJobs( '/tmp/jobs' )
+  job_id = jobs.create( '/tmp/toto' )
+  jobs.start( job_id )
+  print jobs.status( job_id )
+  print jobs.wait( job_id )
 
