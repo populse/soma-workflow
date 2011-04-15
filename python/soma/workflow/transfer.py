44c6cd74550b33e34e4b8e89bed50002e28e05a9
SoizicLaguitton <SoizicLaguitton@613412ce-1361-4b7a-93bf-a3c75af1bae4>
2011-04-15 15:55:18 +0000

    fix

--- python/soma/workflow/transfer.py
+++ python/soma/workflow/transfer.py
@@ -213,24 +213,35 @@ class TransferSCP(Transfer):
   def transfer_to_remote(self, path, remote_path, overwrite=False):
     if os.path.isfile(path):
       scp_cmd = 'scp -C %s "%s@%s:%s"' %(path, 
-                                         user, host, remote_path)
+                                         username, 
+                                         hostname, 
+                                         remote_path)
       print scp_cmd
       os.system(scp_cmd)
 
     if os.path.isdir(path):
-      scp_cmd = 'scp -C -r %s "%s@%s:%s"' %(path, user, host, remote_path)
+      scp_cmd = 'scp -C -r %s "%s@%s:%s"' %(path, 
+                                            username, 
+                                            hostname, 
+                                            remote_path)
       print scp_cmd
       os.system(scp_cmd)
       
 
   def transfer_from_remote(self, remote_path, path, overwrite=False):
     if self.remote_file_controller.is_file(remote_path):
-      scp_cmd = 'scp -C "%s@%s:%s" %s ' %(user, host, remote_path, path)
+      scp_cmd = 'scp -C "%s@%s:%s" %s ' %(username, 
+                                          hostname, 
+                                          remote_path, 
+                                          path)
       print scp_cmd
       os.system(scp_cmd)
       
     if self.remote_file_controller.is_dir(remote_path):
-      scp_cmd = 'scp -C -r "%s@%s:%s" %s ' %(user, host, remote_path, path)
+      scp_cmd = 'scp -C -r "%s@%s:%s" %s ' %(username, 
+                                             hostname, 
+                                             remote_path, 
+                                             path)
       print scp_cmd
       os.system(scp_cmd)
       
