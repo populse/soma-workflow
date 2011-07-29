9ee6b4118e6bee6e26eaeb3f6d2fff6a092c7315
SoizicLaguitton <SoizicLaguitton@613412ce-1361-4b7a-93bf-a3c75af1bae4>
2011-07-29 13:40:08 +0000

    WIP: documentation

--- doc/welcome_page/source/conf.py
+++ doc/welcome_page/source/conf.py
@@ -99,7 +99,7 @@ html_theme = 'sphinxdoc'
 html_theme_options = {"nosidebar":"true"}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -115,7 +115,7 @@ html_title = "soma-workflow"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "images/icon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
