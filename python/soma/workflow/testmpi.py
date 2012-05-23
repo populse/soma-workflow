aa7fa6a7280a04f29ade9736108b151539bd4b4b
bdamota <bdamota@613412ce-1361-4b7a-93bf-a3c75af1bae4>
2012-05-23 09:29:48 +0000

    new testmpi

--- python/soma/workflow/testmpi.py
+++ python/soma/workflow/testmpi.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
 
     comm = MPI.COMM_WORLD
     any_source = MPI.ANY_SOURCE
+    any_tag = MPI.ANY_TAG
     rank = comm.Get_rank()
     size = comm.size
 
@@ -20,22 +21,28 @@ if rank == 0:
         # print 'ask me.'
         send_task = False
         while send_task == False:
-            if comm.Iprobe(source=MPI.ANY_SOURCE, tag=11, status=status):
-                s = status.Get_source()
-                data = comm.recv(source=s, tag=11)
-                # print "receive %s from %d " % (data, s)
-                if data == 'JOB_PLEASE':
-                    comm.send(task, dest=s, tag=12)
-                else:
-                    print 'bad message from %d' % i
-                send_task = True
+            comm.Probe(source=any_source, tag=11, status=status)
+            s = status.Get_source()
+            data = comm.recv(source=s, tag=11)
+            # print "receive %s from %d " % (data, s)
+            if data == 'JOB_PLEASE':
+                comm.send(task, dest=s, tag=12)
+            else:
+                print 'bad message from %d' % s
+            send_task = True
+    for slave in range(1, comm.size):
+        comm.send('STOP', dest=slave, tag=13)
     print "### master ends ###"
 # slave code
 else:
+    status = MPI.Status()
     while True:
         # print 'job_please ', rank
         comm.send('JOB_PLEASE', dest=0, tag=11)
-        data = comm.recv(source=0, tag=12)
-        print 'receive %f from the master. ' % data
-
-
+        comm.Probe(source=any_source, tag=any_tag, status=status)
+        t = status.Get_tag()
+        if t != 13:
+            data = comm.recv(source=0, tag=12)
+            print 'receive %f from the master. ' % data
+        else:
+            break
