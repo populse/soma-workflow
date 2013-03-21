ff842c4fed52814ccd077b8017860219ffca0729
JinpengLI <mr.li.jinpeng@gmail.com>
2013-03-21 09:30:21 +0100

    server script

--- setup_server.py
+++ setup_server.py
@@ -86,7 +86,13 @@ if cur_version < req_version or cur_version >= (3,0):
 end to check the requirement on the server side
 '''
 
-print "hello, this is new version of python"
+print "Hello, this is new version of python"
+os.system("echo ${PATH}")
+os.system("echo ${PYTHONPATH}")
+os.system("echo ${LD_LIBRARY_PATH}")
+os.system("echo ${SOMA_WORKFLOW_EXAMPLES}")
+os.system("echo ${SOMA_WORKFLOW_EXAMPLES_OUT}")
+os.system("echo ${DRMAA_LIBRARY_PATH}")
 sys.exit(0)
 
 
