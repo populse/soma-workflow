39d40ccab2ccda1070f5f3607aff19c7d60a8ba6
LaureHugo <hugolaure@free.fr>
2013-10-30 15:47:08 +0100

    TEST: adding unittest to serialization test

--- python/soma_workflow/test/test_serialization.py
+++ python/soma_workflow/test/test_serialization.py
@@ -1,60 +1,54 @@
 import os
-import sys
 
 from soma_workflow.test import workflow_local
 from soma_workflow.test import workflow_shared
 from soma_workflow.test import workflow_transfer
 from soma_workflow.client import Helper
+import unittest
+
+
+class SerializationTest(unittest.TestCase):
+
+    def test_serialization(self):
+        directory = "/tmp/"
+
+        simple_wf_examples = workflow_local.WorkflowExamplesLocal()
+        tr_wf_examples = workflow_transfer.WorkflowExamplesTransfer()
+        srp_wf_examples = workflow_shared.WorkflowExamplesShared()
+        workflows = []
+        workflows.append(("multiple", simple_wf_examples.example_multiple()))
+        workflows.append(("special_command",
+                          simple_wf_examples.example_special_command()))
+
+        workflows.append(("mutiple_transfer",
+                          tr_wf_examples.example_multiple()))
+        workflows.append(("special_command_transfer",
+                          tr_wf_examples.example_special_command()))
+        workflows.append(("special_transfer",
+                          tr_wf_examples.example_special_transfer()))
+
+        workflows.append(("mutiple_srp", srp_wf_examples.example_multiple()))
+        workflows.append(("special_command_srp",
+                          srp_wf_examples.example_special_command()))
+
+        for workflow_name, workflow in workflows:
+            print "Testing", workflow_name
+
+            file_path = os.path.join(directory,
+                                     "json_" + workflow_name + ".wf")
+            Helper.serialize(file_path, workflow)
+
+            new_workflow = Helper.unserialize(file_path)
+
+            self.assertTrue(new_workflow.attributs_equal(workflow),
+                            "Serialization failed for workflow %s" %
+                            workflow_name)
+
+            try:
+                os.remove(file_path)
+            except IOError:
+                pass
+
 
 if __name__ == '__main__':
-
-    directory = "/tmp/"
-
-    simple_wf_examples = workflow_local.WorkflowExamplesLocal()
-    tr_wf_examples = workflow_transfer.WorkflowExamplesTransfer()
-    srp_wf_examples = workflow_shared.WorkflowExamplesShared()
-    workflows = []
-    workflows.append(("multiple", simple_wf_examples.example_multiple()))
-    workflows.append(("special_command",
-                      simple_wf_examples.example_special_command()))
-
-    workflows.append(("mutiple_transfer", tr_wf_examples.example_multiple()))
-    workflows.append(("special_command_transfer",
-                      tr_wf_examples.example_special_command()))
-    workflows.append(("special_transfer",
-                      tr_wf_examples.example_special_transfer()))
-
-    workflows.append(("mutiple_srp", srp_wf_examples.example_multiple()))
-    workflows.append(("special_command_srp",
-                      srp_wf_examples.example_special_command()))
-
-    failed_workflows = []
-
-    for workflow_name, workflow in workflows:
-        print "--------------------------------------------------------------"
-        print workflow_name
-
-        file_path = os.path.join(directory, "json_" + workflow_name + ".wf")
-        Helper.serialize(file_path, workflow)
-
-        new_workflow = Helper.unserialize(file_path)
-
-        if not new_workflow.attributs_equal(workflow):
-            print "FAILED !!"
-            failed_workflows.append(workflow_name)
-        else:
-            print "OK"
-
-        try:
-            os.remove(file_path)
-        except IOError:
-            pass
-
-    if len(failed_workflows) == 0:
-        print "\nWorkflow serialization successful"
-    else:
-        print "Failed tests :"
-        for failed_workflow in failed_workflows:
-            print "*", failed_workflow
-
-    sys.exit(0)
+    unittest.main()
