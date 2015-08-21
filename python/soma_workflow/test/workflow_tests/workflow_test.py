7f73bde8f74b38a973f01d65ef8f452c1c53543d
Denis Riviere <denis.riviere.gm@gmail.com>
2015-08-21 11:51:14 +0200

    delete temporary database at the end of tests even if no test is actually done.

--- python/soma_workflow/test/workflow_tests/workflow_test.py
+++ python/soma_workflow/test/workflow_tests/workflow_test.py
@@ -58,14 +58,12 @@ class WorkflowTest(unittest.TestCase):
         elif self.path_management == self.SHARED_TRANSFER:
             workflow_examples = WorkflowExamplesSharedTransfer()
         self.wf_examples = workflow_examples
-        # raise Exception("WorkflowTest is an abstract class.")
 
     def tearDown(self):
         if self.wf_id:
             self.__class__.wf_ctrl.delete_workflow(self.wf_id)
         if os.path.isdir(self.wf_examples.output_dir):
             shutil.rmtree(self.wf_examples.output_dir)
-        os.unlink(self.wf_ctrl.config.get_database_file())
 
     @classmethod
     def run_test(cls, debug=False, interactive=False):
@@ -80,13 +78,6 @@ class WorkflowTest(unittest.TestCase):
                              " =================== \n")
             config = Configuration.load_from_file(resource_id,
                                                   config_file_path)
-            # use a temporary sqlite database in soma-workflow to avoid concurrent
-            # access problems
-            tmpdb = tempfile.mkstemp('.db', prefix='swf_')
-            os.close(tmpdb[0])
-            os.unlink(tmpdb[1])
-            config._database_file = tmpdb[1]
-
             if not interactive and config.get_mode() != LIGHT_MODE:
                 sys.stdout.write('Resource %s is not tested in '
                                  'non-interactive mode\n' % resource_id)
@@ -102,45 +93,60 @@ class WorkflowTest(unittest.TestCase):
                     continue
             (login, password) = get_user_id(resource_id, config)
 
-            with suppress_stdout(debug):
-                wf_controller = WorkflowController(resource_id,
-                                                   login,
-                                                   password,
-                                                   config=config)
-                cls.setup_wf_controller(wf_controller)
+            # use a temporary sqlite database in soma-workflow to avoid
+            # concurrent access problems
+            tmpdb = tempfile.mkstemp('.db', prefix='swf_')
+            os.close(tmpdb[0])
+            os.unlink(tmpdb[1])
+            config._database_file = tmpdb[1]
 
-            allowed_config = cls.allowed_config[:]
-            for configuration in cls.allowed_config:
-                if config.get_mode() != configuration[0]:
-                    allowed_config.remove(configuration)
-            if len(allowed_config) == 0:
-                sys.stdout.write("No tests available for the resource %s \n"
-                                 % resource_id)
+            try:
 
-            for configuration in allowed_config:
-                (mode, file_system) = configuration
-                sys.stdout.write("\n---------------------------------------\n")
-                sys.stdout.write("Mode : " + mode + '\n')
-                sys.stdout.write("File system : " + file_system + '\n')
-                cls.setup_path_management(file_system)
-
-                suite_list = []
-                list_tests = []
-                for test in dir(cls):
-                    prefix = "test_"
-                    if len(test) < len(prefix):
-                        continue
-                    if test[0: len(prefix)] == prefix:
-                        list_tests.append(test)
-
-                suite_list.append(unittest.TestSuite(map(cls,
-                                                     list_tests)))
-                alltests = unittest.TestSuite(suite_list)
                 with suppress_stdout(debug):
-                    res = unittest.TextTestRunner(verbosity=2).run(alltests)
+                    wf_controller = WorkflowController(resource_id,
+                                                      login,
+                                                      password,
+                                                      config=config)
+                    cls.setup_wf_controller(wf_controller)
 
-                if len(res.errors) != 0 or len(res.failures) != 0:
-                    sys.exit(1)
+                allowed_config = cls.allowed_config[:]
+                for configuration in cls.allowed_config:
+                    if config.get_mode() != configuration[0]:
+                        allowed_config.remove(configuration)
+                if len(allowed_config) == 0:
+                    sys.stdout.write(
+                        "No tests available for the resource %s \n"
+                        % resource_id)
+
+                for configuration in allowed_config:
+                    (mode, file_system) = configuration
+                    sys.stdout.write(
+                        "\n---------------------------------------\n")
+                    sys.stdout.write("Mode : " + mode + '\n')
+                    sys.stdout.write("File system : " + file_system + '\n')
+                    cls.setup_path_management(file_system)
+
+                    suite_list = []
+                    list_tests = []
+                    for test in dir(cls):
+                        prefix = "test_"
+                        if len(test) < len(prefix):
+                            continue
+                        if test[0: len(prefix)] == prefix:
+                            list_tests.append(test)
+
+                    suite_list.append(unittest.TestSuite(map(cls,
+                                                        list_tests)))
+                    alltests = unittest.TestSuite(suite_list)
+                    with suppress_stdout(debug):
+                        res = unittest.TextTestRunner(verbosity=2).run(
+                            alltests)
+
+                    if len(res.errors) != 0 or len(res.failures) != 0:
+                        sys.exit(1)
+
+            finally:
+                os.unlink(config._database_file)
 
     @staticmethod
     def parse_args(argv):
