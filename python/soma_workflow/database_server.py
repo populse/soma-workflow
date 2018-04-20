fdb7f523d1772a62bfd0f7fb439c722146c52e8a
Denis Rivière <denis.riviere.gm@gmail.com>
2018-04-20 12:07:35 +0200

    improved sqlite3 lib detection, especially for windows

--- python/soma_workflow/database_server.py
+++ python/soma_workflow/database_server.py
@@ -31,6 +31,10 @@ from datetime import datetime
 import socket
 import itertools
 import math
+import glob
+import ctypes
+import ctypes.util
+import tempfile
 
 import soma_workflow.constants as constants
 from soma_workflow.client import FileTransfer, TemporaryPath
@@ -278,6 +282,66 @@ def create_database(database_file):
     connection.close()
 
 
+# -- this is a copy of the find_library in soma-base soma.utils.find_library
+ctypes_find_library = ctypes.util.find_library
+
+def find_library(name):
+    ''' :func:`ctypes.util.find_library` is broken on linux at least: it relies
+    on ``ldconfig``, which only searches system paths, not user paths nor
+    ``LD_LIBRARY_PATH``, or alternatively uses ``gcc``, which is not always
+    installed nor configured.
+
+    Here we are looking in ``[[DY]LD_LIBRARY_]PATH`` (depending on the system)
+    '''
+    def sorted_match(filenames):
+        return sorted(filenames)[-1] # probably not the best
+
+    exts = ['.so']
+    patterns = [ext + '.*' for ext in exts]
+    fname = 'lib' + name
+    if sys.platform.startswith('linux'):
+        envar = 'LD_LIBRARY_PATH'
+    elif sys.platform == 'darwin':
+        envar = 'DYLD_LIBRARY_PATH'
+        exts = ['.dylib']
+        patterns = ['.*' + ext for ext in exts]
+    elif sys.platform.startswith('win'):
+        envar = 'PATH'
+        exts = ['.dll', '.DLL']
+        patterns = ['.*' + ext for ext in exts]
+    else:
+        # other undetermined system (bsd, othe unix...?), assume ELF
+        envar = 'LD_LIBRARY_PATH'
+    paths = os.environ.get(envar)
+    if paths is None:
+        # no path: fallback to ctypes
+        return ctypes_find_library(name)
+
+    paths = paths.split(os.pathsep)
+    names = [fname + ext for ext in exts] + [name + ext for ext in exts]
+    patterns = [fname + pattern for pattern in patterns] \
+        + [name + pattern for pattern in patterns]
+    found = None
+    for path in paths:
+        for tname in names:
+            filename = os.path.join(path, tname)
+            if os.path.exists(filename):
+                found = filename
+                break
+        for tname in patterns:
+            filenames = glob.glob(os.path.join(path, tname))
+            if len(filenames) != 0:
+                found = sorted_match(filenames)
+                break
+
+    if found is not None:
+        return os.path.basename(os.path.realpath(found))
+
+    # not found: fallback to ctypes
+    return ctypes_find_library(name)
+#--
+
+
 _sqlite3_max_variable_number = -1
 
 def sqlite3_max_variable_number():
@@ -295,13 +359,15 @@ def sqlite3_max_variable_number():
         return _sqlite3_max_variable_number
 
     try:
-        import ctypes
-        import tempfile
 
-        t = tempfile.mkstemp(suffix='.sqlite')
-        os.close(t[0])
-        dll = ctypes.CDLL(ctypes.util.find_library('sqlite3'))
+        lib = find_library('sqlite3')
+        if lib is None and sys.platform.startswith('win'):
+            lib = find_library('sqlite3-0')
+        dll = ctypes.CDLL(lib)
+
         if dll is not None:
+            t = tempfile.mkstemp(suffix='.sqlite')
+            os.close(t[0])
             try:
                 db = ctypes.c_void_p(None)
                 dll.sqlite3_open_v2(t[1], ctypes.byref(db), 2,
