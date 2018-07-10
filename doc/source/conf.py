13edeed045909ea36e9e814c5a2b65542ec16a96
Nicolas Souedet <nicolas.souedet@cea.fr>
2018-07-10 16:11:24 +0200

    fixes: doc issue, uses napoleon only when installed

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0,os.path.abspath("../../python/soma_workflow/"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', napoleon]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
 try:
     # try napoleon which replaces numpydoc (and googledoc),
     # comes with sphinx 1.2
